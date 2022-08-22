542b1b60e21a0878df9e37d3cd4b486d989621b4
jess <jcottrill1123@gmail.com>
2022-08-22 12:30:37 -0400

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(99);
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine();
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(37);
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+            Console.WriteLine();
+        }
+    }
+}
